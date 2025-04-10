3f74b5392ea65b0ce4fe8d4b4c17c7b5172dc700
bezlo <sebastiandan@interia.pl>
2025-04-11 00:29:21 +0200

    added window app with only menu bar, and empty folders

---
+++ WPF_App/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DeskManagementStand_App.View.UserControls
+{
+    public partial class MenuBar : UserControl
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF_App/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using DeskManagementStand_App.MVVM;
+
+namespace DeskManagementStand_App.ViewModel
+{
+    class MainWindowViewModel
+    {
+        
+            public ICommand CloseAppCommand { get; }
+
+            public MainWindowViewModel()
+            {
+                CloseAppCommand = new RelayCommand(_ => CloseApp());
+            }
+
+            private void CloseApp()
+            { 
+                Application.Current.Shutdown();
+            }
+        
+    }
+}
