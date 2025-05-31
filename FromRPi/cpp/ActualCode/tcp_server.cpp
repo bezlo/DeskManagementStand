f4869b916ca94269b59aefbaa0735121a5551b13
bezlo <sebastiandan@interia.pl>
2025-05-31 18:14:31 +0200

    I changed order of calling set_rgb_callback() -> check if this is working on rspi

--- FromRPi/cpp/ActualCode/tcp_server.cpp
+++ FromRPi/cpp/ActualCode/tcp_server.cpp
@@ -7,13 +7,14 @@ using boost::asio::ip::tcp;
 tcp_server::tcp_server(boost::asio::io_service& io_service)
     : io_service_(io_service), acceptor_(io_service, tcp::endpoint(tcp::v4(), 5000))
 {
-    start_accept();
+    //start_accept(); is commented cause set_rgb_callback need to be call first
+    //start_accept();
 }
 
 void tcp_server::start_accept()
-{
+{   //tcp_connection::create call setup_commands(), where rgb_callback shouldnt be empty
     tcp_connection::pointer new_connection = tcp_connection::create(io_service_);
-    
+    //with first and every next connection we add right function
     new_connection->set_rgb_callback(rgb_callback);
     
     acceptor_.async_accept(new_connection->socket(),
@@ -26,13 +27,17 @@ void tcp_server::handle_accept(tcp_connection::pointer new_connection,
     const boost::system::error_code& error)
 {
     if (!error) {
+        //just in case call set_rgb_callback again -> check if this is needed
+        new_connection->set_rgb_callback(rgb_callback);
         new_connection->start();
     }
 
     start_accept();
 }
-
+// this function neeed to be call before start_accept();
 void tcp_server::set_rgb_callback(std::function<void(int,int,int)> callback)
 {
     rgb_callback = std::move(callback);
+    //when the callback is setted, start connection
+    start_accept();
 }
