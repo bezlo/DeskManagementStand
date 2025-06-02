a42a37cba25366428003195fad22af3bb2c05e6b
bezlo <sebastiandan@interia.pl>
2025-06-02 13:29:15 +0200

    new design, sort folders in project tree, today goal add communication from pi to app

---
+++ WPF_App/Services/ChargingParametersService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskManagementStand_App.Services
+{
+    internal class ChargingParametersService
+    {
+        private readonly Func<double> _getChargingPower;
+        private readonly Func<double> _getChargingVoltage;
+        private readonly Func<double> _getChargingCurrent;
+
+        public ChargingParametersService(Func<double> getChargingPower, Func<double> getChargingVoltage, Func<double> getChargingCurrent)
+        {
+            _getChargingPower = getChargingPower;
+            _getChargingVoltage = getChargingVoltage;
+            _getChargingCurrent = getChargingCurrent;
+        }
+    }
+}
